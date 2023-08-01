09469b9eac72d5cf7d0207d5bc1895c4100b4614
Dompoo <dlckdrms0517@gmail.com>
2023-08-01 17:02:17 +0900

    8/1 Jpa로 Repository 짜기

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,16 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10) //camal - 언더스코어 자동변환 -> 사실 필요 없음
     private String itemName;
     private Integer price;
     private Integer quantity;
