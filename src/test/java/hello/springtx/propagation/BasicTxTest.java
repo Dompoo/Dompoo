e70a9f9e5bad5bedb4b6f30ae1cae835c9cad871
Dompoo <dlckdrms0517@gmail.com>
2023-08-07 19:48:09 +0900

    8/7 트랜잭션 전파 기본

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -50,4 +50,65 @@ public class BasicTxTest {
         log.info("트랜잭션 롤백 완료");
     }
 
+    @Test
+    void double_commit() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋");
+        txManager.commit(tx2);
+
+        log.info("트랜잭션1,2 커밋 완료");
+    }
+
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 롤백");
+        txManager.rollback(tx2);
+
+        log.info("트랜잭션1 커밋, 트랜잭션2 롤백 완료");
+    }
+
+    /**
+     * 첫번째 트랜잭션(외부)이 시작되고, 끝나기 전에 다음 트랜잭션(내부)이 시작되면,
+     * 내부 트랜잭션이 외부 트랜잭션에 참여하는 것 처럼 작동한다. 따라서 하나의 트랜잭션으로 만들어준다.
+     * 이때 각각의 트랜잭션을 논리 트랜잭션이라고 하고, 하나의 큰 트랜잭션을 물리 트랜잭션이라고 한다.
+     *
+     * 규칙1. 모든 논리 트랜잭션이 커밋되어야 물리 트랜잭션이 전체 커밋된다.
+     * 규칙2. 하나의 논리 트랜잭션이라도 롤백되면 물리 트랜잭션은 전체 롤백된다.
+     */
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        //내부 트랜잭션은 외부 트랜잭션에 '참여' 한다. = 외부 트랜잭션의 범위가 내부 트랜잭션까지 넓어진다.
+        //따라서 외부 트랜잭션과 내부 트랜잭션이 하나의 물리 트랜잭션으로 묶이는 것이다.
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋"); //내부 트랜잭션은 물리 커밋을 하지 않는다.
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋"); //외부 트랜잭션의 커밋에서 물리 커밋이 일어난다.
+        txManager.commit(outer);
+    }
+
+
+
+
 }
