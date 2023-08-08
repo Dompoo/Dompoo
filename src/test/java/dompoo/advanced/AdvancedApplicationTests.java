ea702e1846cf768847db77fe5f8127d0ab7edeae
Dompoo <dlckdrms0517@gmail.com>
2023-08-08 12:26:07 +0900

    8/8 공부 시작

---
+++ src/test/java/dompoo/advanced/AdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package dompoo.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
