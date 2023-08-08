456c904c9e97de703ed0d3af30d0bb5d7fab326a
Dompoo <dlckdrms0517@gmail.com>
2023-08-08 13:31:08 +0900

    8/8 로그 추적기 개발

---
+++ src/test/java/dompoo/advanced/helloTrace/HelloTraceV1Test.java
@@ -0,0 +1,24 @@
+package dompoo.advanced.helloTrace;
+
+import dompoo.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+}
