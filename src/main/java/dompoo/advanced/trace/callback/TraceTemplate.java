be8a65c02cb2635246a4b74e75dc7e61f34f4dcb
Dompoo <dlckdrms0517@gmail.com>
2023-08-09 14:30:51 +0900

    8/9 템플릿 콜백 패턴 적용

---
+++ src/main/java/dompoo/advanced/trace/callback/TraceTemplate.java
@@ -0,0 +1,28 @@
+package dompoo.advanced.trace.callback;
+
+import dompoo.advanced.trace.TraceStatus;
+import dompoo.advanced.trace.logTrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TraceTemplate {
+
+    private final LogTrace trace;
+
+    public <T> T execute(String message, TraceCallback<T> callback) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderService.orderItem()");
+
+            //콜백 실행
+            T result = callback.call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
