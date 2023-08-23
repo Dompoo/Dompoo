4f977efa753a7e95cfb3534e90edd262d1980df4
Dompoo <dlckdrms0517@gmail.com>
2023-08-23 11:46:20 +0900

    8/23 AOP - @Around

--- src/test/java/dompoo/aop/AopTest.java
+++ src/test/java/dompoo/aop/AopTest.java
@@ -2,15 +2,18 @@ package dompoo.aop;
 
 import dompoo.aop.order.OrderRepository;
 import dompoo.aop.order.OrderService;
+import dompoo.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
