81202a85e55f1a43c8168771d7756e1f5a73f9d1
Dompoo <dlckdrms0517@gmail.com>
2023-08-23 12:10:21 +0900

    8/23 AOP - 여러 어드바이스

--- src/test/java/dompoo/aop/AopTest.java
+++ src/test/java/dompoo/aop/AopTest.java
@@ -4,6 +4,7 @@ import dompoo.aop.order.OrderRepository;
 import dompoo.aop.order.OrderService;
 import dompoo.aop.order.aop.AspectV1;
 import dompoo.aop.order.aop.AspectV2;
+import dompoo.aop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopTest {
 
     @Autowired
