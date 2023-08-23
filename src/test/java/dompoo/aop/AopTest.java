d5c8018a9f2b1b885e47ddc974dacd94d5fbe53d
Dompoo <dlckdrms0517@gmail.com>
2023-08-23 12:01:57 +0900

    8/23 AOP - @Pointcut

--- src/test/java/dompoo/aop/AopTest.java
+++ src/test/java/dompoo/aop/AopTest.java
@@ -3,6 +3,7 @@ package dompoo.aop;
 import dompoo.aop.order.OrderRepository;
 import dompoo.aop.order.OrderService;
 import dompoo.aop.order.aop.AspectV1;
+import dompoo.aop.order.aop.AspectV2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV1.class)
+@Import(AspectV2.class)
 public class AopTest {
 
     @Autowired
