51424c725c2bc1c2258ce9345b34e6028f37fb0f
Dompoo <dlckdrms0517@gmail.com>
2023-10-31 22:41:30 +0900

    10/31 전략패턴 공부

---
+++ src/main/java/dompoo/study/strategyPattern/StudyApplication.java
@@ -0,0 +1,33 @@
+package dompoo.study.strategyPattern;
+
+import dompoo.study.strategyPattern.fly.FlyNoWay;
+import dompoo.study.strategyPattern.quack.Squeak;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		Duck healtyDuck = new HealthyDuck();
+		Duck oldDuck = new OldDuck();
+		Duck toyDuck = new ToyDuck();
+
+		healtyDuck.introduce();
+		healtyDuck.fly();
+		healtyDuck.quack();
+
+		oldDuck.introduce();
+		oldDuck.fly();
+		oldDuck.quack();
+
+		toyDuck.introduce();
+		toyDuck.fly();
+		toyDuck.quack();
+
+		log.info("건강한 오리가 총에 맞았습니다! 건강한 오리가 행동합니다.");
+		healtyDuck.setFlyBehavior(new FlyNoWay());
+		healtyDuck.setQuackBehavior(new Squeak());
+		healtyDuck.fly();
+		healtyDuck.quack();
+	}
+}
