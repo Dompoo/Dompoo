f247948dcd67a3ce8bc92aaf0eeac22347010549
Dompoo <dlckdrms0517@gmail.com>
2024-06-20 03:15:14 +0900

    두번째 병합 준비

--- merge_repos.sh
+++ merge_repos.sh
@@ -6,7 +6,7 @@ git add .
 git commit -m "Prepare for adding subtrees"
 
 # 각 레포지토리를 서브트리로 추가 (브랜치 이름이 main인 경우)
-for repo in https://github.com/Dompoo/SpringBoot_Core_Boot.git https://github.com/Dompoo/SpringBoot_Core_Embed.git https://github.com/Dompoo/SpringBoot_Core_Server.git https://github.com/Dompoo/Spring_Core_AOP.git https://github.com/Dompoo/Spring_Core_Proxy.git https://github.com/Dompoo/Spring_Core_Callback.git https://github.com/Dompoo/Spring_DB2_Transaction.git https://github.com/Dompoo/Spring_DB2_DataTech.git; do
+for repo in https://github.com/Dompoo/Study_Spring_Inflearn.git; do
     repo_name=$(basename $repo .git)
     git remote add -f $repo_name $repo
     # master 브랜치 대신 main 브랜치를 사용하는 경우 아래 명령어를 수정하세요.
