691de95b321b37deeff25181514512901e71f6bc
Dompoo <dlckdrms0517@gmail.com>
2024-06-20 00:36:28 +0900

    스프링 핵심 원리 - 비율 할인 정책 적용과 문제점

--- spring-core/SpringCore/src/main/java/Dompoo/SpringCore/order/OrderServiceImpl.java
+++ spring-core/SpringCore/src/main/java/Dompoo/SpringCore/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package Dompoo.SpringCore.order;
 
 import Dompoo.SpringCore.discount.DiscountPolicy;
-import Dompoo.SpringCore.discount.FixDiscountPolicy;
+import Dompoo.SpringCore.discount.RateDiscountPolicy;
 import Dompoo.SpringCore.member.Member;
 import Dompoo.SpringCore.member.MemberRepository;
 import Dompoo.SpringCore.member.MemoryMemberRepository;
@@ -11,7 +11,10 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     //여기서 다른 DiscountPolicy 구현체로 갈아끼울려면 OCP, DIP가 위반된다.
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //OrderServiceImpl이 역할과 구현에 모두 의존하고 있기 때문이다.
+    //이를 해결하기 위해서는 구현체를 '생성하고 주입'하는 역할을 수행하는 다른 무언가가 필요하다!
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
