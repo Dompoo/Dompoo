9608eb8f5a3f61365c25613ce1673a51c1fdadd3
Dompoo <dlckdrms0517@gmail.com>
2024-06-20 01:22:12 +0900

    스프링 핵심 원리 - AppConfig 리팩터링 : 구현체 주입을 메서드로 추출

--- spring-core/SpringCore/src/main/java/Dompoo/SpringCore/AppConfig.java
+++ spring-core/SpringCore/src/main/java/Dompoo/SpringCore/AppConfig.java
@@ -1,6 +1,8 @@
 package Dompoo.SpringCore;
 
+import Dompoo.SpringCore.discount.DiscountPolicy;
 import Dompoo.SpringCore.discount.RateDiscountPolicy;
+import Dompoo.SpringCore.member.MemberRepository;
 import Dompoo.SpringCore.member.MemberService;
 import Dompoo.SpringCore.member.MemberServiceImpl;
 import Dompoo.SpringCore.member.MemoryMemberRepository;
@@ -11,11 +13,21 @@ public class AppConfig {
 
     public MemberService memberService() {
         //생성자를 통해 '의존관계를 주입'한다.
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepositoy());
     }
 
     public OrderService orderService() {
         //생성자를 통해 '의존관계를 주입'한다.
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepositoy(), discountPolicy());
+    }
+
+    public MemberRepository memberRepositoy() {
+        //구현체를 갈아끼우기 위해서는 이 부분만 수행하면 된다.
+        return new MemoryMemberRepository();
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        //구현체를 갈아끼우기 위해서는 이 부분만 수행하면 된다.
+        return new RateDiscountPolicy();
     }
 }
