549baf2b830e508043510efbeb9b1f7e620e39a7
Dompoo <dlckdrms0517@gmail.com>
2024-06-20 10:34:43 +0900

    스프링 핵심 원리 - 스프링 컨테이너 사용

--- spring-core/SpringCore/src/main/java/Dompoo/SpringCore/AppConfig.java
+++ spring-core/SpringCore/src/main/java/Dompoo/SpringCore/AppConfig.java
@@ -8,24 +8,31 @@ import Dompoo.SpringCore.member.MemberServiceImpl;
 import Dompoo.SpringCore.member.MemoryMemberRepository;
 import Dompoo.SpringCore.order.OrderService;
 import Dompoo.SpringCore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         //생성자를 통해 '의존관계를 주입'한다.
         return new MemberServiceImpl(memberRepositoy());
     }
 
+    @Bean
     public OrderService orderService() {
         //생성자를 통해 '의존관계를 주입'한다.
         return new OrderServiceImpl(memberRepositoy(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepositoy() {
         //구현체를 갈아끼우기 위해서는 이 부분만 수행하면 된다.
         return new MemoryMemberRepository();
     }
 
+    @Bean
     private static DiscountPolicy discountPolicy() {
         //구현체를 갈아끼우기 위해서는 이 부분만 수행하면 된다.
         return new RateDiscountPolicy();
