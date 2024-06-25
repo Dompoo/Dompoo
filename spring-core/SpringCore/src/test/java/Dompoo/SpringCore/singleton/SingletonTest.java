26cc6d8af42e9f14194c88748ef34c72181bab40
Dompoo <dlckdrms0517@gmail.com>
2024-06-25 23:46:26 +0900

    스프링 핵심 원리 - 스프링 없는 순수 컨테이너

---
+++ spring-core/SpringCore/src/test/java/Dompoo/SpringCore/singleton/SingletonTest.java
@@ -0,0 +1,25 @@
+package Dompoo.SpringCore.singleton;
+
+import Dompoo.SpringCore.AppConfig;
+import Dompoo.SpringCore.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+    
+    @Test
+    @DisplayName("스프링 없는 순수 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        //계속해서 새로운 객체를 생성한다.
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
