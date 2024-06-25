0b07ee20edd899adfb21947852f023c1ffab63ac
Dompoo <dlckdrms0517@gmail.com>
2024-06-26 00:17:29 +0900

    스프링 핵심 원리 - 싱글톤 패턴 사용시 주의점

---
+++ spring-core/SpringCore/src/test/java/Dompoo/SpringCore/singleton/StatefulSingletonService.java
@@ -0,0 +1,17 @@
+package Dompoo.SpringCore.singleton;
+
+public class StatefulSingletonService {
+
+    private int price;
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + ", price = " + price);
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+}
