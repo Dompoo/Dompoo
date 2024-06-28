61590d817c26381b522d56565368c5831996828d
Dompoo <dlckdrms0517@gmail.com>
2024-06-29 01:01:28 +0900

    스프링 핵심 원리 - 프로토타입 빈 조회시 ObjectProvider 사용

--- spring-core/SpringCore/src/test/java/Dompoo/SpringCore/scope/SingletonWithPrototype.java
+++ spring-core/SpringCore/src/test/java/Dompoo/SpringCore/scope/SingletonWithPrototype.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -39,6 +40,19 @@ public class SingletonWithPrototype {
         //예상치 못한 상황! 우리의 의도는 로직을 호출할 때 마다 새로 생성하고 싶다.
     }
 
+    @Test
+    void singletonUseProvider() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBeanWithProvider.class, PrototypeBean.class);
+        ClientBeanWithProvider cleint1 = ac.getBean(ClientBeanWithProvider.class);
+        int count1 = cleint1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBeanWithProvider client2 = ac.getBean(ClientBeanWithProvider.class);
+        int count2 = client2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+        //이제는 로직을 수행할 때마다 프로토타입 빈을 생성한다.
+    }
+
     @Scope("singleton")
     static class ClientBean {
         private final PrototypeBean protoTypeBean;
@@ -51,6 +65,19 @@ public class SingletonWithPrototype {
         public int logic() {
             protoTypeBean.addCount();
             return protoTypeBean.getCount();
+        }
+    }
+
+    @Scope("singleton")
+    static class ClientBeanWithProvider {
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
         }
     }
 
