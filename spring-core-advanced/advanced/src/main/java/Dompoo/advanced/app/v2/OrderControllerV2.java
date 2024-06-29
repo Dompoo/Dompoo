880cb8e5059b72ad4b17d66442887e5ebfe8c7f3
Dompoo <dlckdrms0517@gmail.com>
2024-06-29 13:52:41 +0900

    스프링 핵심 원리 : 고급편 - 로그추적기 V2 적용(파라미터로 상태 넘기기)

---
+++ spring-core-advanced/advanced/src/main/java/Dompoo/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package Dompoo.advanced.app.v2;
+
+import Dompoo.advanced.app.trace.TraceStatus;
+import Dompoo.advanced.app.trace.hellotrace.HelloTraceV1;
+import Dompoo.advanced.app.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("orderController.request");
+            //계속해서 파라미터로 TraceId를 넘겨야 한다. -> 수정해야 하는 클래스가 오억개라면?
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "OK";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 던져주어야 애플리케이션 흐름을 바꾸지 않는 것!
+        }
+    }
+}
