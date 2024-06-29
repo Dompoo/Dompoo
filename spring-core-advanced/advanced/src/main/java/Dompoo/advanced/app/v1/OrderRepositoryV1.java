b78866dda62a6f4127067f01a63c83eaf5d8cfce
Dompoo <dlckdrms0517@gmail.com>
2024-06-29 13:41:51 +0900

    스프링 핵심 원리 : 고급편 - 로그추적기 V1 적용(상태 저장 X)

---
+++ spring-core-advanced/advanced/src/main/java/Dompoo/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,36 @@
+package Dompoo.advanced.app.v1;
+
+import Dompoo.advanced.app.trace.TraceStatus;
+import Dompoo.advanced.app.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("orderRepository.save");
+            sleep(1000);
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
