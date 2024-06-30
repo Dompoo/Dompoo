7b5c9e1a0c9c49911848201fae533dbcceed8439
Dompoo <dlckdrms0517@gmail.com>
2024-06-30 23:50:17 +0900

    스프링 핵심 원리 : 고급편 - 전략패턴 기본 구조

---
+++ spring-core-advanced/advanced/src/test/java/Dompoo/advanced/app/strategy/ContextV1Test.java
@@ -0,0 +1,52 @@
+package Dompoo.advanced.app.strategy;
+
+import Dompoo.advanced.app.strategy.code.strategy.ContextV1;
+import Dompoo.advanced.app.strategy.code.strategy.StrategyLogic1;
+import Dompoo.advanced.app.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void stategyV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    @Test
+    void stategyV1() {
+        //spring의 의존관계 주입도 이와 똑같다.
+
+        StrategyLogic1 logic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(logic1);
+        context1.execute();
+
+        StrategyLogic2 logic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(logic2);
+        context2.execute();
+    }
+}
