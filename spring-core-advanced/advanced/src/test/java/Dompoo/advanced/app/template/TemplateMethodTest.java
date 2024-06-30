dea87fc1959f461ced0c3b94fa58c8fca12ead36
Dompoo <dlckdrms0517@gmail.com>
2024-06-30 22:55:22 +0900

    스프링 핵심 원리 : 고급편 - 템플릿메서드패턴 기본 구조

--- spring-core-advanced/advanced/src/test/java/Dompoo/advanced/app/template/TemplateMethodTest.java
+++ spring-core-advanced/advanced/src/test/java/Dompoo/advanced/app/template/TemplateMethodTest.java
@@ -8,6 +8,7 @@ public class TemplateMethodTest {
 
     //핵심 로직과 비핵심 로직이 나뉘어있지 않다.
     //비핵심 로직(횡단관심사)를 좀 더 분리할 수 있는 방법이 없을까?
+    //비핵심 로직은 변하지 않는 코드이며, 핵심 로직은 여러 클래스에 걸쳐 바뀌게 된다.
 
     @Test
     void templateMethodV0() {
@@ -35,5 +36,15 @@ public class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
+    }
+
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        //AbstractTemplate의 execute를 실행하다가,
+        //call을 만나면 call은 오버라이딩된 SubClass의 call을 실행하게 된다.
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 }
