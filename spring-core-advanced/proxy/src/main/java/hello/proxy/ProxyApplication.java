19cf3deca7ecf4e3be02fb22a38600cedccaa5f3
Dompoo <dlckdrms0517@gmail.com>
2024-07-01 17:03:03 +0900

    스프링 핵심 원리 : 고급편 - v3(구체 클래스만을 컴포넌트 스캔) 클래스 작성

--- spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV2Config.class) //스프링 빈에 등록하기 위해서 사용한다.
+@Import({AppV1Config.class, AppV2Config.class}) //스프링 빈에 등록하기 위해서 사용한다.
 //모든 V1, 2, 3를 등록하지 않고, 선택적으로 등록하기 위해서 사용
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
