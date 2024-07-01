288a87509e02047ad5986cbc392d5533f7ea1c9a
Dompoo <dlckdrms0517@gmail.com>
2024-07-01 16:50:16 +0900

    스프링 핵심 원리 : 고급편 - v1(인터페이스와 구체 클래스를 수동 등록) 클래스 작성

--- spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,12 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppV1Config.class) //스프링 빈에 등록하기 위해서 사용한다.
+//모든 V1, 2, 3를 등록하지 않고, 선택적으로 등록하기 위해서 사용
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
