0aff9c7070242d7a4a154357f717ebefc1b7b99e
Dompoo <dlckdrms0517@gmail.com>
2024-07-02 01:38:09 +0900

    스프링 핵심 원리 : 고급편 - 데코레이터패턴을 예제에 적용하여 부가기능 추가 2

--- spring-core-advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ spring-core-advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +20,16 @@ public class DecoratorPatternTest {
         Component component = new RealComponent();
         Component decorator = new MessageDecorator(component);
         DecoratorPatternClient client = new DecoratorPatternClient(decorator);
+
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component component = new RealComponent();
+        Component decorator1 = new MessageDecorator(component);
+        Component decorator2 = new TimeDecorator(decorator1);
+        DecoratorPatternClient client = new DecoratorPatternClient(decorator2);
 
         client.execute();
     }
