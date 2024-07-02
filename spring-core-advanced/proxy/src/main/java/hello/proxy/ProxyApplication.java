91f751002c5c96ed7b7c1bd0084a050e68cb8b9a
Dompoo <dlckdrms0517@gmail.com>
2024-07-02 14:18:11 +0900

    스프링 핵심 원리 : 고급편 - v1(인터페이스와 구체클래스를 수동 등록)에 프록시 적용

--- spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,18 +1,25 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class}) //스프링 빈에 등록하기 위해서 사용한다.
+@Import(InterfaceProxyConfig.class) //스프링 빈에 등록하기 위해서 사용한다.
 //모든 V1, 2, 3를 등록하지 않고, 선택적으로 등록하기 위해서 사용
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
