a3419ba2edde69c92b8150cbf1cf53f12eec9066
Dompoo <dlckdrms0517@gmail.com>
2024-07-06 02:04:57 +0900

    스프링 핵심 원리 : AOP - 여러 종류의 어드바이스

--- spring-core-aop/aop/src/test/java/dompoo/aop/AopTest.java
+++ spring-core-aop/aop/src/test/java/dompoo/aop/AopTest.java
@@ -2,7 +2,7 @@ package dompoo.aop;
 
 import dompoo.aop.order.OrderRepository;
 import dompoo.aop.order.OrderService;
-import dompoo.aop.order.aop.AspectV5Order;
+import dompoo.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
