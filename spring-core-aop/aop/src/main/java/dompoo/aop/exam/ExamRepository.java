1fec5329c60cc341e54697a046340c6d5d1f302a
Dompoo <dlckdrms0517@gmail.com>
2024-07-06 18:30:25 +0900

    스프링 핵심 원리 : AOP - Log AOP 추가

--- spring-core-aop/aop/src/main/java/dompoo/aop/exam/ExamRepository.java
+++ spring-core-aop/aop/src/main/java/dompoo/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package dompoo.aop.exam;
 
+import dompoo.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -8,6 +9,7 @@ public class ExamRepository {
     private static int seq = 0;
 
     //5번에 1번 실패하는 요청
+    @Trace
     public String save(String itemId) {
         seq++;
         if (seq % 5 == 0) {
--- spring-core-aop/aop/src/test/java/dompoo/aop/exam/ExamTest.java
+++ spring-core-aop/aop/src/test/java/dompoo/aop/exam/ExamTest.java
@@ -1,12 +1,15 @@
 package dompoo.aop.exam;
 
+import dompoo.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Slf4j
+@Import(TraceAspect.class)
 public class ExamTest {
 
     @Autowired
