e6c2d39ddfbcfb9caeb898622092d8f3ac136164
Dompoo <dlckdrms0517@gmail.com>
2024-07-09 16:46:24 +0900

    스프링 MVC 1편 : 회원 저장폼, 저장, 리스트조회 서블릿 개발

---
+++ spring-mvc-1/servlet/src/main/java/dompoo/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,44 @@
+package dompoo.servlet.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter w = response.getWriter();
+        String html =
+                "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "    username: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                "    <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n";
+        w.write(html);
+    }
+}
+/*
+서블릿으로 개발하면 기본적으로 제공되는 request, response를 통해
+편하게 HTTP 요청을 처리할 수 있다.
+하지만, HTML을 생성하는 코드가 매우 복잡하고 지저분하다.
+-> JSP
+ */
