62c54dfdd75000e1b008fb1e45c43fe9579e5b93
Dompoo <dlckdrms0517@gmail.com>
2024-07-13 01:36:32 +0900

    init : 코프링 데모 프로젝트 시작

---
+++ src/test/kotlin/dompoo/kopringdemo/KopringDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dompoo.kopringdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
