d3c0812e3c11ecac7235ed2993cf1fe6558da432
Dompoo <dlckdrms0517@gmail.com>
2024-07-13 22:52:54 +0900

    feat : mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,10 +26,17 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
+
+	// kotest
+	testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+	testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+	testImplementation("io.kotest:kotest-property:5.9.1")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+
+	// mockk
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
-	testImplementation ("io.kotest:kotest-assertions-core:5.9.1")
-	testImplementation ("io.kotest:kotest-property:5.9.1")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
@@ -43,7 +50,3 @@ kotlin {
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
 }
-
-//tasks.withType<Test> {
-//	useJUnitPlatform()
-//}
