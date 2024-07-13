534b01bb9f1cdd7b8c593ad2f74225c8aabf5870
Dompoo <dlckdrms0517@gmail.com>
2024-07-13 22:53:08 +0900

    feat : mockk를 사용한 데모 테스트 작성

--- src/test/kotlin/dompoo/kopringdemo/service/MemberServiceTest.kt
+++ src/test/kotlin/dompoo/kopringdemo/service/MemberServiceTest.kt
@@ -1,10 +1,22 @@
 package dompoo.kopringdemo.service
 
+import com.ninjasquad.springmockk.MockkBean
+import dompoo.kopringdemo.domain.Member
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.mockk
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
 
-class MemberServiceTest : BehaviorSpec({
-	Context("이런 컨텍스트에서") {
+@SpringBootTest
+class MemberServiceTest : BehaviorSpec() {
+
+	@MockkBean
+	val memrepository = mockk<MemberRepository>()
+	@MockkBean
+	val memberService = mockk<MemberService>()
+
+	init {
 		Given("이런게 주어졌을 때,") {
 			When("이렇게 하면,") {
 				Then("이렇게 되어야 한다.") {
@@ -13,4 +25,13 @@ class MemberServiceTest : BehaviorSpec({
 			}
 		}
 	}
-})
+
+	fun createMember(
+		username: String = "dompoo",
+		birth: LocalDate = LocalDate.of(2000, 5, 17)
+	): Member =
+		Member(
+			username = username,
+			birth = birth
+		)
+}
