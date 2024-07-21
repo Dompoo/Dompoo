af50bdec48d74de93096930580d735c67d894c12
Dompoo <dlckdrms0517@gmail.com>
2024-07-21 17:11:33 +0900

    여름 스프링 스터디 : 컨트롤러 어드바이스 데모 프로젝트 init

---
+++ spring/controller-advice-demo/src/test/java/dompoo/controller_advice_demo/ControllerAdviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dompoo.controller_advice_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerAdviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
