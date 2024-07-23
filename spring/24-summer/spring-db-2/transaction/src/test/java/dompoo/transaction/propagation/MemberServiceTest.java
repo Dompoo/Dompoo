3bc6379e3720d3b87c3aced03f05e0fe730aa466
Dompoo <dlckdrms0517@gmail.com>
2024-07-23 12:31:36 +0900

    스프링 DB 2편 : 직렬적 트랜잭션에서 하나만 롤백

--- spring/24-summer/spring-db-2/transaction/src/test/java/dompoo/transaction/propagation/MemberServiceTest.java
+++ spring/24-summer/spring-db-2/transaction/src/test/java/dompoo/transaction/propagation/MemberServiceTest.java
@@ -3,6 +3,7 @@ package dompoo.transaction.propagation;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +25,7 @@ class MemberServiceTest {
     MemberService
     MemberRepository    @Transactional
     LogRepository       @Transactional
+    둘다 저장
      */
     @Test
     void outerTxOff_success() {
@@ -36,6 +38,27 @@ class MemberServiceTest {
         //then 둘다 저장
         assertThat(memberRepository.findByUsername(username)).isPresent();
         assertThat(logRepository.findByMessage(username)).isPresent();
+    }
+    
+    /*
+    MemberService
+    MemberRepository    @Transactional
+    LogRepository       @Transactional  Exception 발생
+    Log는 롤백, Member만 저장
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "outerTxOff_fail_로그예외";
+        
+        //when
+        Assertions.assertThatThrownBy(() ->
+                memberService.joinV1(username))
+            .isExactlyInstanceOf(RuntimeException.class);
+        
+        //then 둘다 저장
+        assertThat(memberRepository.findByUsername(username)).isPresent();
+        assertThat(logRepository.findByMessage(username)).isEmpty();
     }
     
 }
