e4399887926ee67398c90415e7ec52c9702f445e
Dompoo <dlckdrms0517@gmail.com>
2024-08-03 13:13:00 +0900

    Practical Testing : Spring Rest Docs 작성하기

---
+++ java/practical-testing/cafekiosk/src/test/java/dompoo/cafekiosk/spring/docs/RestDocsSupport.java
@@ -0,0 +1,32 @@
+package dompoo.cafekiosk.spring.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(RestDocumentationExtension.class)
+//@SpringBootTest
+public abstract class RestDocsSupport {
+	
+	protected MockMvc mockMvc;
+	protected ObjectMapper objectMapper = new ObjectMapper();
+	
+	@BeforeEach
+	void setUp(
+//			WebApplicationContext context,
+			RestDocumentationContextProvider provider
+	) {
+		this.mockMvc = MockMvcBuilders
+//				.webAppContextSetup(context)
+				.standaloneSetup(initController())
+				.apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+				.build();
+	}
+	
+	protected abstract Object initController();
+}
