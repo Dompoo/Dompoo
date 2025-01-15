956d40cf091b78e43814b40a0601fd8bd95aff1b
이창근 <dlckdrms0517@gmail.com>
2025-01-15 23:07:58 +0900

    homebrew 애플리케이션 백업 스크립트 작성

---
+++ 기타/homebrew_backup_script.sh
@@ -0,0 +1,27 @@
+# Homebrew 설치
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Homebrew Formulae 설치
+brew install git
+
+# Homebrew Casks 설치
+brew install --cask \
+    alt-tab iina \
+    postman \
+    appcleaner \
+    intellij-idea \
+    bartender \
+    itsycal \
+    raycast \
+    devtoys \
+    jetbrains-toolbox \
+    rectangle \
+    discord \
+    keka \
+    slack \
+    docker \
+    maccy \
+    visual-studio-code \
+    google-chrome \
+    mysqlworkbench \
+    warp
