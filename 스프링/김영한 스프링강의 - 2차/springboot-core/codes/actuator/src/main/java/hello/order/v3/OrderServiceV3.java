2b4ed75f11811714e15851b5d179afe26d2cd600
Dompoo <dlckdrms0517@gmail.com>
2025-02-06 19:47:34 +0900

    서비스 메트릭 - Timed

---
+++ 스프링/김영한 스프링강의 - 2차/springboot-core/codes/actuator/src/main/java/hello/order/v3/OrderServiceV3.java
@@ -0,0 +1,32 @@
+package hello.order.v3;
+
+import hello.order.OrderService;
+import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class OrderServiceV3 implements OrderService {
+    
+    private AtomicInteger stock = new AtomicInteger(100);
+    
+    @Override
+    @Timed("my.order")
+    public void order() {
+        log.info("주문");
+        stock.decrementAndGet();
+    }
+    
+    @Override
+    @Timed("my.order")
+    public void cancel() {
+        log.info("취소");
+        stock.incrementAndGet();
+    }
+    
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+}
